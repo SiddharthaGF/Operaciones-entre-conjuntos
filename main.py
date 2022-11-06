c543e5e23c27e785964fd8566d5f72741171d449
SiddharthaGF <pablo.guerra@espoch.edu.ec>
2022-11-05 21:23:30 -0500

    final

--- main.py
+++ main.py
@@ -1,153 +1,214 @@
-from asyncio.windows_events import NULL
 from os import system
-import string
 from conjutos import Conjuntos
-
+import string
 
 def ImprimirConjuntos(conjuntos: list[set]) -> None:
+
     i = 65
+
     for conjunto in conjuntos:
-        print(f'Conjunto {chr(i)} = {conjunto}')
+        letra = chr(i)
+        print(f'{letra} = {conjunto}')
         i += 1
+
     print('\n')
 
 
-def IntentarConvertir(cadena: string) -> int or string or NULL:
+def TryParseIntOrString(cadena: string) -> int or string or float or None:
+
+    if cadena == "":
+        return None
+
     try:
-        return int(cadena)
+        if '.' not in cadena:
+            return int(cadena)
+
+        else:
+            return float(cadena)
+
     except:
-        if len(cadena) == 0:
-            return NULL
         return cadena
 
 
-def ComprovarRepeticion(conjunto: set, elemento) -> bool:
-    return elemento in conjunto
-
-
 def LeerConjunto(letra: string) -> set:
-    n = CantidadElementos(letra)
+
     conjunto = set()
+    n = CantidadElementosConjuntos(letra)
     i = 1
-    while len(conjunto) < n:
-        aux = input(f'\nIngrese el elemento {i} del conjunto {letra} >> ')
-        elemento = IntentarConvertir(aux)
-        if not elemento:
+
+    while i <= n:
+
+        elemento = input(f'\nIngrese el elemento {i} del conjunto {letra} >> ')
+        aux = TryParseIntOrString(elemento)
+
+        if aux == None:
             print('No puede ingresar datos en blanco.')
-        elif ComprovarRepeticion(conjunto, elemento):
+
+        elif aux in conjunto:
             print('El elemento ya existe en el conjunto.')
+
         else:
-            conjunto.add(elemento)
+            conjunto.add(aux)
             i += 1
+
     return conjunto
 
 
-def CantidadElementos(letra: string):
+def CantidadElementosConjuntos(letra: string) -> int:
+
     while True:
+
         try:
-            n = int(
-                input(f'\nIngrese la cantidad de elementos del conjunto {letra} >> '))
+            n = int(input(f'Ingrese el número de elementos {letra} >> '))
+
             if n < 0:
                 print("La cantidad de elementos del conjunto no puede ser 0")
+
             elif n > 10:
                 print("La cantidad maxima de elementos permitida es 10.")
+
             else:
                 return n
+
         except:
             print("Solo se adminten valores enteros.")
 
 
 def CantidadConjuntos() -> int:
+
     while True:
+
         try:
+
             n = int(input("\nIngrese la cantidad de conjuntos >> "))
+
             if n < 2:
-                print("Se requieren al menos 2 conjuntos para operar.")
+                print("Se requiere al menos 2 conjuntos para operar.")
+
             elif n > 5:
-                print("La cantidad maxima de conjuntos permitida es 5.")
+                print("La cantidad máxima de conjuntos permitida es 5.")
+
             else:
                 return n
+
         except:
+
             print("Solo se adminten valores enteros.")
 
 
-def SelecionarOperacion(opc: int) -> None:
+def CrearConjuntos(n: int) -> list[set]:
+
     lista_conjuntos = list()
+
+    for i in range(0, n):
+        letra = chr(i+65)
+        conjunto = LeerConjunto(letra)
+        lista_conjuntos.append(conjunto)
+
+    return lista_conjuntos
+
+
+def OperacionUnion(lista_conjuntos: list[set]) -> None:
+    conjunto_union = Conjuntos.Union(*lista_conjuntos)
+    ImprimirConjuntos(lista_conjuntos)
+    print(f'Unión = {conjunto_union}')
+
+
+def OperacionIntersccion(lista_conjuntos: list[set]) -> None:
+    conjunto_interseccion = Conjuntos.Intersecion(*lista_conjuntos)
+    ImprimirConjuntos(lista_conjuntos)
+    print(f'Interseción = {conjunto_interseccion}')
+
+
+def OperacionSuma(lista_conjuntos: list[set]) -> None:
+    conjunto_suma = Conjuntos.Suma(*lista_conjuntos)
+    ImprimirConjuntos(lista_conjuntos)
+    print(f'Suma A + B = {conjunto_suma}')
+
+
+def OperacionDiferencia(lista_conjuntos: list[set]) -> None:
+    ImprimirConjuntos(lista_conjuntos)
+    conjunto_diferencia_ab = Conjuntos.Diferencia(*lista_conjuntos)
+    print(f'Diferencia A - B = {conjunto_diferencia_ab}')
+
+
+def SelecionarOperacion(opc: int) -> None:
+
+    system('cls')
+
     match opc:
+
         case 1:
-            system('cls')
+
             n = CantidadConjuntos()
-            for i in range(1, n+1):
-                conjunto = LeerConjunto(chr(i+64))
-                lista_conjuntos.append(conjunto)
-            conjunto_union = Conjuntos.Union(*lista_conjuntos)
-            system('cls')
-            ImprimirConjuntos(lista_conjuntos)
-            print(f'Unión = {conjunto_union}')
+            conjuntos = CrearConjuntos(n)
+            OperacionUnion(conjuntos)
+
         case 2:
-            system('cls')
+
             n = CantidadConjuntos()
-            for i in range(1, n+1):
-                conjunto = LeerConjunto(chr(i+64))
-                lista_conjuntos.append(conjunto)
-            conjunto_interseccion = Conjuntos.Intersecion(*lista_conjuntos)
-            system('cls')
-            ImprimirConjuntos(lista_conjuntos)
-            print(f'Interseción = {conjunto_interseccion}')
+            conjuntos = CrearConjuntos(n)
+            OperacionIntersccion(conjuntos)
+
         case 3:
-            system('cls')
-            print("  Dos conjuntos oblgatorio")
-            conjuntoA = LeerConjunto(chr(65))
-            conjuntoB = LeerConjunto(chr(66))
-            system('cls')
-            ImprimirConjuntos(lista_conjuntos)
-            conjunto_suma = Conjuntos.Suma(conjuntoA, conjuntoB)
-            print(f'Suma A + B = {conjunto_suma}')
+
+            print("  Para esta operación se requiere exactamente 2 conjuntos (Suma)")
+            conjuntos = CrearConjuntos(2)
+            OperacionSuma(conjuntos)
+
         case 4:
-            system('cls')
-            print("  Dos conjuntos oblgatorio")
-            conjuntoA = LeerConjunto(chr(65))
-            conjuntoB = LeerConjunto(chr(66))
-            system('cls')
-            ImprimirConjuntos(lista_conjuntos)
-            conjunto_diferencia_ab = Conjuntos.Diferencia(conjuntoA, conjuntoB)
-            print(f'Diferencia A - B = {conjunto_diferencia_ab}')
-            conjunto_diferencia_bc = Conjuntos.Diferencia(conjuntoB, conjuntoA)
-            print(f'Diferencia B - A = {conjunto_diferencia_bc}')
-        case _:
-            system('cls')
-            print("\t\tNos vemos!\n\n")
+
+            print("  Para esta operación se requiere exactamente 2 conjuntos (Diferencia)")
+            conjuntos = CrearConjuntos(2)
+            OperacionDiferencia(conjuntos)
 
 
 def LeerOpcion() -> int:
+
     while True:
+
         try:
-            opc = int(input("\nIngrese su opcion >> "))
+
+            opc = int(input("\nIngrese su opción >> "))
+
             if opc >= 0 and opc < 5:
                 return opc
+
             else:
-                print("Opción no valida.")
+                print("Opción no válida.")
+
         except:
+
             print("Solo se adminten valores enteros.")
 
 
 def PreguntarContinuar() -> bool:
-    print("\nDesea realiar otra operacion?")
+
+    print("\nDesea realizar otra operacion?")
     print("  1.- Si")
     print("  2.- No")
+
     while True:
+
         try:
+
             opc = int(input("\nIngrese su opcion >> "))
+
             if opc >= 1 and opc <= 2:
                 return opc == 1
+
             else:
                 print("Opción no valida.")
+
         except:
+
             print("Solo se adminten valores enteros.")
 
 
 def Inicio() -> None:
+
     continuar = True
+
     while continuar:
         system('cls')
         print("\t**** CALCULADORA DE CONJUNTOS ****\n")
@@ -160,6 +221,8 @@ def Inicio() -> None:
         opc = LeerOpcion()
         SelecionarOperacion(opc)
         continuar = PreguntarContinuar()
+
+    print("\t\tNos vemos!\n\n")
 
 
 Inicio()
--- test_conjuntos.py
+++ test_conjuntos.py
@@ -8,107 +8,181 @@ from conjutos import Conjuntos
 class TestConjuntos(unittest.TestCase):
 
     def test_union_1(self):
+
         conjutoA = set({2, 3})
         conjutoB = set({3, 4, (5, 0)})
-        self.assertEqual(Conjuntos.Union(
-            conjutoA, conjutoB), {2, 3, 4, (5, 0)})
+
+        respuesta = Conjuntos.Union(conjutoA, conjutoB)
+        esperado = {2, 3, 4, (5, 0)}
+
+        self.assertEqual(respuesta, esperado)
 
     def test_union_2(self):
-        conjutoA = set({1, 'a'})
 
+        conjutoA = set({1, 'a'})
         conjutoB = set({2, 'b'})
-        self.assertEqual(Conjuntos.Union(conjutoA, conjutoB), {'a', 'b', 1, 2})
+
+        respuesta = Conjuntos.Union(conjutoA, conjutoB)
+        esperado = {'a', 'b', 1, 2}
+
+        self.assertEqual(respuesta, esperado)
 
     def test_union_3(self):
+
         conjutoA = set({1, 2, 3})
         conjutoB = set({3, 4, 5})
         conjutoC = set({0})
-        self.assertEqual(Conjuntos.Union(
-            conjutoA, conjutoB, conjutoC), {0, 1, 2, 3, 4, 5})
+
+        respuesta = Conjuntos.Union(conjutoA, conjutoB, conjutoC)
+        esperado = {0, 1, 2, 3, 4, 5}
+
+        self.assertEqual(respuesta, esperado)
 
     def test_union_5(self):
+
         conjutoA = set({True, 2, 3})
         conjutoB = set({3, 4, False})
-        self.assertEqual(Conjuntos.Union(conjutoA, conjutoB),
-                         {False, True, 2, 3, 4})
+
+        respuesta = Conjuntos.Union(conjutoA, conjutoB)
+        esperado = {False, True, 2, 3, 4}
+
+        self.assertEqual(respuesta, esperado)
 
     def test_union_conmutativa(self):
+
         conjutoA = set({1, 2, 3})
         conjutoB = set({3, 4, 5})
-        self.assertEqual(Conjuntos.Union(conjutoA, conjutoB),
-                         Conjuntos.Union(conjutoB, conjutoA))
+
+        respuesta1 = Conjuntos.Union(conjutoA, conjutoB)
+        respuesta2 = Conjuntos.Union(conjutoB, conjutoA)
+        esperado = {1, 2, 3, 4, 5}
+
+        self.assertEqual(respuesta1, esperado)
+        self.assertEqual(respuesta2, esperado)
 
     def test_union_asociativa(self):
+
         conjutoA = set({1, 2, 3})
         conjutoB = set({3, 4, 5})
         conjutoC = set({1, 2, 3, 4})
+
         unionAB = Conjuntos.Union(conjutoA, conjutoB)
         unionABC = Conjuntos.Union(unionAB, conjutoC)
+
         unionBC = Conjuntos.Union(conjutoB, conjutoC)
         unionBCA = Conjuntos.Union(unionBC, conjutoA)
+
         self.assertEqual(unionABC, unionBCA)
 
     def test_Intersecion_1(self):
-        conjutoA = set({True, 2, 3})
-        conjutoB = set({3, 4, False})
-        self.assertEqual(Conjuntos.Intersecion(conjutoA, conjutoB),
-                         {3})
+
+        conjutoA = set({'a', 'b', 'c'})
+        conjutoB = set({1, 2, 3, 4, 5})
+        conjutoC = set({'by', 'de', 4})
+
+        respuesta = Conjuntos.Intersecion(conjutoA, conjutoB, conjutoC)
+        esperado = set()
+
+        self.assertEqual(respuesta, esperado)
 
     def test_Intersecion_2(self):
+
         conjutoA = set({})
         conjutoB = set({0})
-        self.assertEqual(Conjuntos.Intersecion(conjutoA, conjutoB),
-                         set())
+
+        respuesta = Conjuntos.Intersecion(conjutoA, conjutoB)
+        esperado = set()
+
+        self.assertEqual(respuesta, esperado)
 
     def test_Intersecion_3(self):
+
         conjutoA = set({1, 2, 3})
         conjutoB = set({3, 4, 5})
         conjutoC = set({0, 3})
+
         intersecionABC = Conjuntos.Intersecion(conjutoA, conjutoB, conjutoC)
         intersecionBCA = Conjuntos.Intersecion(conjutoB, conjutoC, conjutoA)
+
         self.assertEqual(intersecionABC, intersecionBCA)
 
     def test_interseccion_conmutativa(self):
+
         conjutoA = set({1, 2, 3})
         conjutoB = set({3, 4, 5})
-        self.assertEqual(Conjuntos.Intersecion(conjutoA, conjutoB),
-                         Conjuntos.Intersecion(conjutoB, conjutoA))
+
+        respuesta1 = Conjuntos.Intersecion(conjutoA, conjutoB)
+        respuesta2 = Conjuntos.Intersecion(conjutoB, conjutoA)
+
+        self.assertEqual(respuesta1, respuesta2)
 
     def test_Intersecion_asociativa(self):
+
         conjutoA = set({1, 2, 3})
         conjutoB = set({3, 4, 5})
         conjutoC = set({1, 2, 3, 4})
+
         intersecionAB = Conjuntos.Intersecion(conjutoA, conjutoB)
         intersecionABC = Conjuntos.Intersecion(intersecionAB, conjutoC)
+
         intersecionBC = Conjuntos.Intersecion(conjutoB, conjutoC)
         intersecionBCA = Conjuntos.Intersecion(intersecionBC, conjutoA)
+
         self.assertEqual(intersecionABC, intersecionBCA)
 
     def test_suma_1(self):
+
         conjutoA = set({1, 2, 3})
         conjutoB = set({3, 4, 5})
-        self.assertEqual(Conjuntos.Suma(conjutoA, conjutoB), {1, 2, 4, 5})
+
+        respuesta = Conjuntos.Suma(conjutoA, conjutoB)
+        esperado = {1, 2, 4, 5}
+
+        self.assertEqual(respuesta, esperado)
+
 
     def test_diferencia_1(self):
+
         conjutoA = set({1, 2, 3})
         conjutoB = set({3, 4, 5})
-        self.assertNotEqual(Conjuntos.Diferencia(conjutoA, conjutoB),
-                            Conjuntos.Diferencia(conjutoB, conjutoA))
+
+        respuesta = Conjuntos.Diferencia(conjutoA, conjutoB)
+        esperado = {1, 2}
+        
+        self.assertEqual(respuesta, esperado)
+
+    def test_diferencia_1(self):
+
+        conjutoA = set({1, 2, 3})
+        conjutoB = set({3, 4, 5})
+
+        diferenciaAB = Conjuntos.Diferencia(conjutoA, conjutoB)
+        diferenciaBA = Conjuntos.Diferencia(conjutoB, conjutoA)
+
+        self.assertNotEqual(diferenciaAB, diferenciaBA)
 
     def test_suma_2(self):
-        conjutoA = set({1, 2, 3})
-        conjutoB = set({3, 4, 5})
-        self.assertEqual(Conjuntos.Suma(conjutoA, conjutoB), {1, 2, 4, 5})
 
-    def test_suma_2(self):
+        conjutoA = set({'a', 'b', 'c'})
+        conjutoB = set({'d', 'b'})
+
+        respuesta = Conjuntos.Suma(conjutoA, conjutoB)
+        esperado = {'a', 'c', 'd'}
+
+        self.assertEqual(respuesta, esperado)
+
+    def test_suma_3(self):
+
         conjutoA = set({1, 2, 3})
         conjutoB = set({3, 4, 5})
+
         suma = Conjuntos.Suma(conjutoA, conjutoB)
         intersecion = Conjuntos.Intersecion(conjutoA, conjutoB)
         union = Conjuntos.Union(conjutoA, conjutoB)
-        diferencia_union_interseccion = Conjuntos.Diferencia(
-            union, intersecion)
-        self.assertEqual(suma, diferencia_union_interseccion)
+
+        diferencia = Conjuntos.Diferencia(union, intersecion)
+
+        self.assertEqual(suma, diferencia)
 
 
 if __name__ == '__main__':
